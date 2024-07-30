28405d0d17bdc600fd0711d64bcd6f51698f1b82
arligle <0774543002@163.com>
2024-07-30 08:34:06 +0800

    feat(git): add:husky commitizen commitlint/cli lint-staged biomejs && created libs/data-modeling

--- apps/master-api/src/app.service.ts
+++ apps/master-api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@aio/data-modeling/index';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Master!';
+    return 'Hello Master!' + add(11, 22);
   }
 }
